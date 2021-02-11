2f98e6733408edd2343bddf801542cbe1e29afe8
Thomseeen <thwa1@web.de>
2021-02-11 19:36:22 +0100

    Properly dynamic behaviour

--- MainView.xaml.cs
+++ MainView.xaml.cs
@@ -9,19 +9,22 @@ namespace mc_statstalker {
 
         public MainView() {
             DataContext = controller;
+
             InitializeComponent();
+
+            ServerField.Text = controller.ServerUri;
+        }
+
+        private void SetButton_Click(object sender, RoutedEventArgs e) {
+            controller.SetServerUri(ServerField.Text);
         }
 
         private async void AddButton_Click(object sender, RoutedEventArgs e) {
-            await controller.AddPlayerStatsAsync(@"https://the-wagner.de/stats/5f3f25ee-4cad-4160-ab23-fd4087869608.json");
-            await controller.AddPlayerStatsAsync(@"https://the-wagner.de/stats/9aecff5d-33cb-43f3-b279-85821fa84d18.json");
-            await controller.AddPlayerStatsAsync(@"https://the-wagner.de/stats/834139bf-8770-45de-a877-3f5b664810c8.json");
-            await controller.AddPlayerStatsAsync(@"https://the-wagner.de/stats/bc1b47ac-f7fb-493b-9a0c-84bf74c22c55.json");
-            await controller.AddPlayerStatsAsync(@"https://the-wagner.de/stats/bdcae066-b237-4543-a90d-72f56daf4af3.json");
+            await controller.AddPlayerStatsAsync(UuidField.Text);
         }
 
         private void RemoveButton_Click(object sender, RoutedEventArgs e) {
-
+            controller.RemovePlayerStats(StatsGrid.SelectedItem);
         }
     }
 }
--- StatsController.cs
+++ StatsController.cs
@@ -1,22 +1,26 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace mc_statstalker {
     public class StatsController : INotifyPropertyChanged {
+        public string ServerUri { get; private set; } = @"https://the-wagner.de/stats/";
         public ObservableCollection<PlayerStatModel> PlayerStats { get; private set; } = new ObservableCollection<PlayerStatModel>();
 
-        public async Task AddPlayerStatsAsync(string uri) {
-            var stats = new PlayerStatModel(uri);
+        public void SetServerUri(string uri) {
+            ServerUri = uri;
+        }
+
+        public async Task AddPlayerStatsAsync(string uuid) {
+            var stats = new PlayerStatModel($"{ServerUri.TrimEnd('/')}/{uuid.TrimStart('/').TrimEnd('/')}.json");
             if (await stats.ReadStatsAsync()) {
                 PlayerStats.Add(stats);
             }
         }
 
-        public void RemovePlayerStats(string uri) {
-            PlayerStats.Remove(PlayerStats.Where(stat => stat.Uri.AbsoluteUri == uri).Single());
+        public void RemovePlayerStats(object item) {
+            PlayerStats.Remove(item as PlayerStatModel);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
